b240746804685a8f440ad6f84e9a02c9f3e701a4
Eric Fischer <ericf712@gmail.com>
2015-08-30 02:50:58 -0700

    Lays out template for result.js controller

--- client/javascript/results.js
+++ client/javascript/results.js
@@ -4,6 +4,26 @@
   
   app.controller('ResultsController', ['$scope', function($scope) {
 
+    $scope.$on('results', function(event, response) {
+      if (response === 'No direct flights available to this location') {
+        $scope.errorMessage = 'No direct flights available to this location';
+      }
+      console.log('Reponse got all the way to results.js ', response); 
+      // $scope.origin = resObj.origin;
+      // $scope.destination = resObj.destination;
+      // $scope.departuredate = resObj.departuredate;
+      // $scope.departuretime = resObj.departuretime;
+      // $scope.arrivaldate = resObj.arrivaldate;
+      // $scope.arrivaltime = resObj.arrivaltime;
+      // $scope.totalflighttime = resObj.totalflighttime;
+      // $scope.price = resObj.price;
+      // $scope.airline = resObj.airline;
+      // $scope.link = resObj.link;
+    });
+
+    // $scope.displayResults = function(resObj) {
+    // }
+
   }]);
 
 })();

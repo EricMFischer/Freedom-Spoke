8ccce7ef3805e0d3aab627544cfb9d21b02a300a
Eric Fischer <ericf712@gmail.com>
2015-09-02 18:04:57 -0700

    Adds ng-repeat in results.html for various flight results

--- client/javascript/search.js
+++ client/javascript/search.js
@@ -1,28 +1,10 @@
 (function() {
 
   var app = angular.module('Search', []);
-
-  app.factory('Flights', ['$http', function($http) {
-    var obj = {};
-    // obj.getFlights = function(from, to, when) {
-    //   console.log('Got to getFlights in Flights Factory');
-    //   return $http.post('/api/flights', {
-    //     from: from,
-    //     to: to,
-    //     when: when
-    //   })
-    //   .success(function(data) {
-    //     // using Angular $http service to query our flights route
-    //     // success cb executes when request returns
-    //     // route returns a list of flights
-    //     obj.flights = data;
-    //   });
-    // };
-    return obj;
-  }]);
   
 
-  app.controller('SearchController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
+  app.controller('SearchController', ['$scope', '$rootScope', '$http', 
+    function($scope, $rootScope, $http) {
 
     $scope.master = {};
     $scope.update = function(search) {
@@ -48,6 +30,6 @@
       });
     };
 
-  }]);
+  }]); // closes controller
 
 })();

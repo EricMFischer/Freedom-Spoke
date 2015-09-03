3834ef66e09be4a88bbaf74f081a50c3a189df43
Eric Fischer <ericf712@gmail.com>
2015-09-02 22:04:09 -0700

    Formats results.js to make leg info belong to leg key on flight object

--- client/javascript/results.js
+++ client/javascript/results.js
@@ -6,57 +6,57 @@
 
     var tripsArr = [ 
     { originCode: 'EVV',
-      origin: 'Evansville',
-      originAirportName: 'Evansville Dress Regional',
-      destinationCode: 'HOU',
-      destination: 'Houston',
-      destinationAirportName: 'Houston William P. Hobby',
-      price: '$317.60',
-      duration: 289, // 8 items up to here
-      legStart1: 'EVV',
-      legEnd1: 'ORD',
-      legDuration1: 75,
-      legCarrier1: 'UA',
-      legFlightNumber1: '4610',
-      legDepartureDate1: '2015-10-10',
-      legDepartureTime1: '15:35',
-      legArrivalDate1: '2015-10-10',
-      legArrivalTime1: '16:50',
-      legStart2: 'ORD',
-      legEnd2: 'IAH',
-      legDuration2: 164,
-      legCarrier2: 'UA',
-      legFlightNumber2: '262',
-      legDepartureDate2: '2015-10-10',
-      legDepartureTime2: '17:40',
-      legArrivalDate2: '2015-10-10',
-      legArrivalTime2: '20:24' }, // 26 items per trip object
-    { originCode: 'EVV',
-      origin: 'Evansville',
-      originAirportName: 'Evansville Dress Regional',
-      destinationCode: 'HOU',
-      destination: 'Houston',
-      destinationAirportName: 'Houston William P. Hobby',
-      price: '$317.60',
-      duration: 290,
-      legStart1: 'EVV',
-      legEnd1: 'DFW',
-      legDuration1: 136,
-      legCarrier1: 'US',
-      legFlightNumber1: '3598',
-      legDepartureDate1: '2015-10-10',
-      legDepartureTime1: '14:04',
-      legArrivalDate1: '2015-10-10',
-      legArrivalTime1: '16:20',
-      legStart2: 'DFW',
-      legEnd2: 'HOU',
-      legDuration2: 69,
-      legCarrier2: 'US',
-      legFlightNumber2: '5785',
-      legDepartureDate2: '2015-10-10',
-      legDepartureTime2: '17:45',
-      legArrivalDate2: '2015-10-10',
-      legArrivalTime2: '18:54' } ];
+    origin: 'Evansville',
+    originAirportName: 'Evansville Dress Regional',
+    destinationCode: 'HOU',
+    destination: 'Houston',
+    destinationAirportName: 'Houston William P. Hobby',
+    price: '$317.60',
+    duration: 5,
+    legStart1: 'EVV',
+    legEnd1: 'ORD',
+    legDuration1: 1,
+    legCarrier1: 'UA',
+    legFlightNumber1: '4642',
+    legDepartureDate1: '2015-10-10',
+    legDepartureTime1: '10:24',
+    legArrivalDate1: '2015-10-10',
+    legArrivalTime1: '11:39',
+    legStart2: 'ORD',
+    legEnd2: 'IAH',
+    legDuration2: 3,
+    legCarrier2: 'UA',
+    legFlightNumber2: '436',
+    legDepartureDate2: '2015-10-10',
+    legDepartureTime2: '12:20',
+    legArrivalDate2: '2015-10-10',
+    legArrivalTime2: '15:09' },
+  { originCode: 'EVV',
+    origin: 'Evansville',
+    originAirportName: 'Evansville Dress Regional',
+    destinationCode: 'HOU',
+    destination: 'Houston',
+    destinationAirportName: 'Houston William P. Hobby',
+    price: '$317.60',
+    duration: 5,
+    legStart1: 'EVV',
+    legEnd1: 'DFW',
+    legDuration1: 2,
+    legCarrier1: 'US',
+    legFlightNumber1: '3598',
+    legDepartureDate1: '2015-10-10',
+    legDepartureTime1: '14:04',
+    legArrivalDate1: '2015-10-10',
+    legArrivalTime1: '16:20',
+    legStart2: 'DFW',
+    legEnd2: 'IAH',
+    legDuration2: 1,
+    legCarrier2: 'US',
+    legFlightNumber2: '2326',
+    legDepartureDate2: '2015-10-10',
+    legDepartureTime2: '17:30',
+    legArrivalDate2: '2015-10-10',
+    legArrivalTime2: '18:42' } ];
 
     $scope.plane = 'https://cdn0.iconfinder.com/data/icons/gcons-2/22/airplane1-48.png';
     $scope.origin = tripsArr[0].origin;
@@ -71,69 +71,52 @@
       obj.duration = trip.duration;
 
       if (Object.keys(trip).length === 17) {
-        obj.legs = trip.legStart1 + ' to ' + trip.legEnd1;
-        obj.legDurations = trip.legDuration1;
-        obj.legCarriers = trip.legCarrier1;
-        obj.legFlightNumbers = trip.legFlightNumber1;
-        obj.legDepartureDates = trip.legDepartureDate1;
-        obj.legDepartureTimes = trip.legDepartureTime1;
-        obj.legArrivalDates = trip.legArrivalDate1;
-        obj.legArrivalTimes = trip.legArrivalTime1;
+        obj.legs = [[trip.legStart1, trip.legEnd1, trip.legDuration1, trip.legCarrier1, trip.legFlightNumber1, trip.legDepartureDate1, trip.legDepartureTime1, trip.legArrivalDate1, trip.legArrivalTime1]];
       }
 
       if (Object.keys(trip).length === 26) {
-        obj.legs = trip.legStart1 + ' to ' + trip.legEnd1 + ' - ' + trip.legStart2 + ' to ' + trip.legEnd2;
-        obj.legDurations = trip.legDuration1 + ', ' + trip.legDuration2;
-        obj.legCarriers = trip.legCarrier1 + ', ' + trip.legCarrier2;
-        obj.legFlightNumbers = trip.legFlightNumber1 + ', ' + trip.legFlightNumber2;
-        obj.legDepartureDates = trip.legDepartureDate1 + ', ' + trip.legDepartureDate2;
-        obj.legDepartureTimes = trip.legDepartureTime1 + ', ' + trip.legDepartureTime2;
-        obj.legArrivalDates = trip.legArrivalDate1 + ', ' + trip.legArrivalDate2;
-        obj.legArrivalTimes = trip.legArrivalTime1 + ', ' + trip.legArrivalTime2;
+        obj.legs = [
+        [trip.legStart1, trip.legEnd1, trip.legDuration1, trip.legCarrier1, trip.legFlightNumber1, trip.legDepartureDate1, trip.legDepartureTime1, trip.legArrivalDate1, trip.legArrivalTime1], 
+        [trip.legStart2, trip.legEnd2, trip.legDuration2, trip.legCarrier2, trip.legFlightNumber2, trip.legDepartureDate2, trip.legDepartureTime2, trip.legArrivalDate2, trip.legArrivalTime2]
+        ];
       }
 
       if (Object.keys(trip).length === 35) {
-        obj.legs = trip.legStart1 + ' to ' + trip.legEnd1 + ' - ' + trip.legStart2 + ' to ' + trip.legEnd2 + ' - ' + trip.legStart3 + ' to ' + trip.legEnd3;
-        obj.legDurations = trip.legDuration1 + ', ' + trip.legDuration2;
-        obj.legCarriers = trip.legCarrier1 + ', ' + trip.legCarrier2 + ', ' + trip.legCarrier3;
-        obj.legFlightNumbers = trip.legFlightNumber1 + ', ' + trip.legFlightNumber2 + ', ' + trip.legFlightNumber3;
-        obj.legDepartureDates = trip.legDepartureDate1 + ', ' + trip.legDepartureDate2 + ', ' + trip.legDepartureDate3;
-        obj.legDepartureTimes = trip.legDepartureTime1 + ', ' + trip.legDepartureTime2 + ', ' + trip.legDepartureTime3;
-        obj.legArrivalDates = trip.legArrivalDate1 + ', ' + trip.legArrivalDate2 + ', ' + trip.legArrivalDate3;
-        obj.legArrivalTimes = trip.legArrivalTime1 + ', ' + trip.legArrivalTime2 + ', ' + trip.legArrivalTime3;
+        obj.legs = [
+        [trip.legStart1, trip.legEnd1, trip.legDuration1, trip.legCarrier1, trip.legFlightNumber1, trip.legDepartureDate1, trip.legDepartureTime1, trip.legArrivalDate1, trip.legArrivalTime1], 
+        [trip.legStart2, trip.legEnd2, trip.legDuration2, trip.legCarrier2, trip.legFlightNumber2, trip.legDepartureDate2, trip.legDepartureTime2, trip.legArrivalDate2, trip.legArrivalTime2], 
+        [trip.legStart3, trip.legEnd3, trip.legDuration3, trip.legCarrier3, trip.legFlightNumber3, trip.legDepartureDate3, trip.legDepartureTime3, trip.legArrivalDate3, trip.legArrivalTime3]
+        ];
       }
 
       if (Object.keys(trip).length === 44) {
-        obj.legs = trip.legStart1 + ' to ' + trip.legEnd1 + ' - ' + trip.legStart2 + ' to ' + trip.legEnd2 + ' - ' + trip.legStart3 + ' to ' + trip.legEnd3 + ' - ' + trip.legStart4 + ' to ' + trip.legEnd4;
-        obj.legDurations = trip.legDuration1 + ', ' + trip.legDuration2;
-        obj.legCarriers = trip.legCarrier1 + ', ' + trip.legCarrier2 + ', ' + trip.legCarrier3 + ', ' + trip.legCarrier4;
-        obj.legFlightNumbers = trip.legFlightNumber1 + ', ' + trip.legFlightNumber2 + ', ' + trip.legFlightNumber3 + ', ' + trip.legFlightNumber4;
-        obj.legDepartureDates = trip.legDepartureDate1 + ', ' + trip.legDepartureDate2 + ', ' + trip.legDepartureDate3 + ', ' + trip.legDepartureDate4;
-        obj.legDepartureTimes = trip.legDepartureTime1 + ', ' + trip.legDepartureTime2 + ', ' + trip.legDepartureTime3 + ', ' + trip.legDepartureTime4;
-        obj.legArrivalDates = trip.legArrivalDate1 + ', ' + legArrivalDate2 + ', ' + trip.legArrivalDate3 + ', ' + trip.legArrivalDate4;
-        obj.legArrivalTimes = trip.legArrivalTime1 + ', ' + legArrivalTime2 + ', ' + trip.legArrivalTime3 + ', ' + trip.legArrivalTime4;
+        obj.legs = [
+        [trip.legStart1, trip.legEnd1, trip.legDuration1, trip.legCarrier1, trip.legFlightNumber1, trip.legDepartureDate1, trip.legDepartureTime1, trip.legArrivalDate1, trip.legArrivalTime1], 
+        [trip.legStart2, trip.legEnd2, trip.legDuration2, trip.legCarrier2, trip.legFlightNumber2, trip.legDepartureDate2, trip.legDepartureTime2, trip.legArrivalDate2, trip.legArrivalTime2], 
+        [trip.legStart3, trip.legEnd3, trip.legDuration3, trip.legCarrier3, trip.legFlightNumber3, trip.legDepartureDate3, trip.legDepartureTime3, trip.legArrivalDate3, trip.legArrivalTime3],
+        [trip.legStart4, trip.legEnd4, trip.legDuration4, trip.legCarrier4, trip.legFlightNumber4, trip.legDepartureDate4, trip.legDepartureTime4, trip.legArrivalDate4, trip.legArrivalTime4] 
+        ];
       }
 
       if (Object.keys(trip).length === 53) {
-        obj.legs = trip.legStart1 + ' to ' + trip.legEnd1 + ' - ' + trip.legStart2 + ' to ' + trip.legEnd2 + ' - ' + trip.legStart3 + ' to ' + trip.legEnd3 + ' - ' + trip.legStart4 + ' to ' + trip.legEnd4 + ' - ' + trip.legStart5 + ' to ' + trip.legEnd5;
-        obj.legDurations = trip.legDuration1 + ', ' + trip.legDuration2;
-        obj.legCarriers = trip.legCarrier1 + ', ' + trip.legCarrier2 + ', ' + trip.legCarrier3 + ', ' + trip.legCarrier4 + ', ' + trip.legCarrier5;
-        obj.legFlightNumbers = trip.legFlightNumber1 + ', ' + trip.legFlightNumber2 + ', ' + trip.legFlightNumber3 + ', ' + trip.legFlightNumber4 + ', ' + trip.legFlightNumber5;
-        obj.legDepartureDates = trip.legDepartureDate1 + ', ' + trip.legDepartureDate2 + ', ' + trip.legDepartureDate3 + ', ' + trip.legDepartureDate4 + ', ' + trip.legDepartureDate5;
-        obj.legDepartureTimes = trip.legDepartureTime1 + ', ' + trip.legDepartureTime2 + ', ' + trip.legDepartureTime3 + ', ' + trip.legDepartureTime4 + ', ' + trip.legDepartureTime5;
-        obj.legArrivalDates = trip.legArrivalDate1 + ', ' + legArrivalDate2 + ', ' + trip.legArrivalDate3 + ', ' + trip.legArrivalDate4 + ', ' + trip.legArrivalDate5;
-        obj.legArrivalTimes = trip.legArrivalTime1 + ', ' + legArrivalTime2 + ', ' + trip.legArrivalTime3 + ', ' + trip.legArrivalTime4 + ', ' + trip.legArrivalTime5;
+        obj.legs = [
+        [trip.legStart1, trip.legEnd1, trip.legDuration1, trip.legCarrier1, trip.legFlightNumber1, trip.legDepartureDate1, trip.legDepartureTime1, trip.legArrivalDate1, trip.legArrivalTime1], 
+        [trip.legStart2, trip.legEnd2, trip.legDuration2, trip.legCarrier2, trip.legFlightNumber2, trip.legDepartureDate2, trip.legDepartureTime2, trip.legArrivalDate2, trip.legArrivalTime2], 
+        [trip.legStart3, trip.legEnd3, trip.legDuration3, trip.legCarrier3, trip.legFlightNumber3, trip.legDepartureDate3, trip.legDepartureTime3, trip.legArrivalDate3, trip.legArrivalTime3],
+        [trip.legStart4, trip.legEnd4, trip.legDuration4, trip.legCarrier4, trip.legFlightNumber4, trip.legDepartureDate4, trip.legDepartureTime4, trip.legArrivalDate4, trip.legArrivalTime4],
+        [trip.legStart5, trip.legEnd5, trip.legDuration5, trip.legCarrier5, trip.legFlightNumber5, trip.legDepartureDate5, trip.legDepartureTime5, trip.legArrivalDate5, trip.legArrivalTime5] 
+        ];
       }
 
       if (Object.keys(trip).length === 62) {
-        obj.legs = trip.legStart1 + ' to ' + trip.legEnd1 + ' - ' + trip.legStart2 + ' to ' + trip.legEnd2 + ' - ' + trip.legStart3 + ' to ' + trip.legEnd3 + ' - ' + trip.legStart4 + ' to ' + trip.legEnd4 + ' - ' + trip.legStart5 + ' to ' + trip.legEnd5 + ' - ' + trip.legStart6 + ' to ' + trip.legEnd6;
-        obj.legDurations = trip.legDuration1 + ', ' + trip.legDuration2;
-        obj.legCarriers = trip.legCarrier1 + ', ' + trip.legCarrier2 + ', ' + trip.legCarrier3 + ', ' + trip.legCarrier4 + ', ' + trip.legCarrier5 + ', ' + trip.legCarrier6;
-        obj.legFlightNumbers = trip.legFlightNumber1 + ', ' + trip.legFlightNumber2 + ', ' + trip.legFlightNumber3 + ', ' + trip.legFlightNumber4 + ', ' + trip.legFlightNumber5 + ', ' + trip.legFlightNumber6;
-        obj.legDepartureDates = trip.legDepartureDate1 + ', ' + trip.legDepartureDate2 + ', ' + trip.legDepartureDate3 + ', ' + trip.legDepartureDate4 + ', ' + trip.legDepartureDate5 + ', ' + trip.legDepartureDate6;
-        obj.legDepartureTimes = trip.legDepartureTime1 + ', ' + trip.legDepartureTime2 + ', ' + trip.legDepartureTime3 + ', ' + trip.legDepartureTime4 + ', ' + trip.legDepartureTime5 + ', ' + trip.legDepartureTime6;
-        obj.legArrivalDates = trip.legArrivalDate1 + ', ' + legArrivalDate2 + ', ' + trip.legArrivalDate3 + ', ' + trip.legArrivalDate4 + ', ' + trip.legArrivalDate5 + ', ' + trip.legArrivalDate6;
-        obj.legArrivalTimes = trip.legArrivalTime1 + ', ' + legArrivalTime2 + ', ' + trip.legArrivalTime3 + ', ' + trip.legArrivalTime4 + ', ' + trip.legArrivalTime5 + ', ' + trip.legArrivalTime6;
+        obj.legs = [
+        [trip.legStart1, trip.legEnd1, trip.legDuration1, trip.legCarrier1, trip.legFlightNumber1, trip.legDepartureDate1, trip.legDepartureTime1, trip.legArrivalDate1, trip.legArrivalTime1], 
+        [trip.legStart2, trip.legEnd2, trip.legDuration2, trip.legCarrier2, trip.legFlightNumber2, trip.legDepartureDate2, trip.legDepartureTime2, trip.legArrivalDate2, trip.legArrivalTime2], 
+        [trip.legStart3, trip.legEnd3, trip.legDuration3, trip.legCarrier3, trip.legFlightNumber3, trip.legDepartureDate3, trip.legDepartureTime3, trip.legArrivalDate3, trip.legArrivalTime3],
+        [trip.legStart4, trip.legEnd4, trip.legDuration4, trip.legCarrier4, trip.legFlightNumber4, trip.legDepartureDate4, trip.legDepartureTime4, trip.legArrivalDate4, trip.legArrivalTime4],
+        [trip.legStart5, trip.legEnd5, trip.legDuration5, trip.legCarrier5, trip.legFlightNumber5, trip.legDepartureDate5, trip.legDepartureTime5, trip.legArrivalDate5, trip.legArrivalTime5],
+        [trip.legStart6, trip.legEnd6, trip.legDuration6, trip.legCarrier6, trip.legFlightNumber6, trip.legDepartureDate6, trip.legDepartureTime6, trip.legArrivalDate6, trip.legArrivalTime6] 
+        ];
       }
 
       $scope.flights.push(obj);

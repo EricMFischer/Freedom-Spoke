aecda9964e6ff9f981c53906dd9e055310b7af3e
Eric Fischer <ericf712@gmail.com>
2015-09-07 19:34:57 -0700

    Creates signin and signup html pages

--- client/app.js
+++ client/app.js
@@ -57,12 +57,12 @@
   })
 
   // here inside the run phase of angular, our services and controllers
-  // have just been registered and our app is ready
-  // however, we want to make sure the user is authorized
-  // we listen for when angular is trying to change routes
+  // have just been registered and our app is ready.
+  // however, we want to make sure the user is authorized.
+  // we listen for when angular is trying to change routes.
   // when it does change routes, we then look for the token in localstorage
   // and send that token to the server to see if it is a real user or hasn't expired
-  // if it's not valid, we then redirect back to signin/signup
+  // if it's invalid, we'll redirect back to signin/signup
   /*
   .run(function ($rootScope, $state, UserFactory) {
     $rootScope.$on('$stateChangeStart', function(event, next) {

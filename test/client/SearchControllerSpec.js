1627e2db011f53fd14f9c549581c5aeab7dd9ffc
Eric Fischer <ericf712@gmail.com>
2015-09-11 16:29:35 -0700

    Adds travis file, karma file, and tests

---
+++ test/client/SearchControllerSpec.js
@@ -0,0 +1,29 @@
+var expect = chai.expect;
+
+describe('ProfileController', function() {
+  var $scope, $rootScope;
+
+  beforeEach(module('Search'));
+
+  beforeEach(inject(function($injector) {
+    $rootScope = $injector.get('$rootScope');
+
+    $scope = $rootScope.$new();
+
+    var $controller = $injector.get('$controller');
+
+    createController = function() {
+      return $controller('SearchController', {
+        $scope: $scope
+      });
+    };
+
+    createController();
+  }));
+
+  // checking for test is just a test to test the tests
+  // we'll probably remove eventually
+  it('should have a username property on the $scope.user', function() {
+    expect($scope.user.username).to.be.a('string');
+  });
+});
